aa62a94af61653b3b9bb3a78d811b1c4dc60a20e
michaelhakansson <michael.hakansson@gmail.com>
2015-03-23 10:41:30 +0100

    Remove duplicate code

--- assets/js/controllers/listListController.js
+++ assets/js/controllers/listListController.js
@@ -37,22 +37,6 @@ todoApp.controller('ListListController',['$http','$log','$scope',function($http,
 	});
 
 
-	io.socket.on('itemRemoved', function(obj){
-		var index = -1;
-		// Find index of the removed item
-		_.each($scope.itemList, function(data, idx) {
-			if (_.isEqual(data.id, parseInt(obj.id))) {
-				index = idx;
-				return;
-			}
-		});
-		// Remove item from the view
-		$scope.itemList.splice(index, 1);
-		// Call $scope.$digest to make the changes in UI
-		$scope.$digest();
-	});
-
-
 	$scope.addList = function() {
 		io.socket.post('/list/addlist/', {name: $scope.listName, collaborators: 2});
 		$scope.listName = "";
